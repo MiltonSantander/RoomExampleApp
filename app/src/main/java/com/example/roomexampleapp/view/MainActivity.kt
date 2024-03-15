ef57e0bf366fcb3568cd5d0f037fd0671bb03d57
MiltonGarcia <milgarsan@gmail.com>
2024-03-15 13:15:23 -0300

    The DataBinding and ViewModel logic was implemented in the MainActivity class and in its view.

--- app/src/main/java/com/example/roomexampleapp/view/MainActivity.kt
+++ app/src/main/java/com/example/roomexampleapp/view/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.roomexampleapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.example.roomexampleapp.R
+import com.example.roomexampleapp.SubscriberViewModel
+import com.example.roomexampleapp.SubscriberViewModelFactory
+import com.example.roomexampleapp.databinding.ActivityMainBinding
+import com.example.roomexampleapp.db.SubscriberDAO
+import com.example.roomexampleapp.db.SubscriberDatabase
+import com.example.roomexampleapp.db.SubscriberRepository
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        subscriberViewModel = ViewModelProvider(
+            this,
+            SubscriberViewModelFactory(
+                SubscriberRepository(
+                    SubscriberDatabase.getInstance(application).subscriberDAO
+                )
+            )
+        )[SubscriberViewModel::class.java]
+        binding.subscriberViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
     }
 }
